d8a824c20d89c5ed554a2875ce775b1cada3677c
“Aryan <aaryanjjadhav555@gmail.com>
2026-08-26 19:15:48 +0530

    create and connect with database

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wander_list';
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch(() => {
+    console.log("failed to connect" + err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => { //after sending a GET request to /, respond with msg.
     res.send("Hi, I am root");
 });
 
-app.listen(8080, () => {
+app.listen(8080, () => {  //Start the server and listen for requests on port 8080.
     console.log("Server is listening on port : 8080");
 });
