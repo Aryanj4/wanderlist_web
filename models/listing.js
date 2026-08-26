91663f3bbe62a5784e08b54d32be3d69f2542208
“Aryan <aaryanjjadhav555@gmail.com>
2026-08-26 20:30:40 +0530

    changes in the sehema

--- models/listing.js
+++ models/listing.js
@@ -2,12 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingScheme = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+
+    },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        set: (v) => v === "" ? "https://images.pexels.com/photos/35784826/pexels-photo-35784826.jpeg" : v
+    },
     price: Number,
     location: String,
     country: String
 })
 
 const Listing = mongoose.model("Listing", listingSchema);
+modules.export = Listing;
