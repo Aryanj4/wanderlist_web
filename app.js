546b58c3c8eaa4815c4c5d12f76b6e236f7fc3cd
“Aryan <aaryanjjadhav555@gmail.com>
2026-09-11 00:08:08 +0530

    'create route' adding data in database

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wander_list";
 
@@ -30,20 +30,28 @@ app.get("/", (req, res) => {
 
 //Index Route
 app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
 //New Route
 app.get("/listings/new", async (req, res) => {
-    res.render("listings/new.ejs");
+  res.render("listings/new.ejs");
 });
 
 //Show Route
 app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  //let {title, description, image, price, country, location} = req.body;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req, res) => {
