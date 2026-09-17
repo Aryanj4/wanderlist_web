86f19d15acc87128dba77b4ef4c7827403ad3ebb
“Aryan <aaryanjjadhav555@gmail.com>
2026-09-17 17:22:59 +0530

    created public folder for static pages. created 'style.css' and linked in 'boilerplate.ejs' to give common styling

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('ejs', ejsMate);
 
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));  //for static files in public folder
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wander_list";
 
